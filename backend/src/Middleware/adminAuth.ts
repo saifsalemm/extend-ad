4c801141b4aca7c70c4eec0b220dff8a4cf5c00c
Saif Salem <saifsalemmail@gmail.com>
2024-10-24 09:58:53 +0300

    new: middleware

---
+++ backend/src/Middleware/adminAuth.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import { RequestWithUser, UserObj } from "../Utils/types.js";
+
+export const adminAuth = async (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res
+        .status(401)
+        .json({ message: "Authorization token missing or malformed" });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const secret = process.env.JWT_SECRET as string;
+    const decoded = jwt.verify(token, secret) as UserObj;
+
+    if (decoded.role !== "admin") {
+      res.status(403).json({ message: "Unauthorized" });
+      return;
+    }
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      res.status(401).json({ message: "Token has expired" });
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      res.status(401).json({ message: "Invalid token" });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+};
