5ffa2668a75070a53fb2c4875d87fbeda21aecd6
Saif Salem <saifsalemmail@gmail.com>
2024-10-28 09:27:56 +0300

    new: frontend initial setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import "./index.css";
+import AccountPage from "./pages/AccountPage/AccountPage";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import CreateProduct from "./pages/AdminPage/CreateProduct/CreateProduct";
+import EditProduct from "./pages/AdminPage/EditProduct/EditProduct";
+import CataloguePage from "./pages/CataloguePage/CataloguePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { useAppSelector } from "./store/hooks";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export enum RoutesEnum {
+  CATALOGUE = "/",
+  ADMIN = "/admin",
+  PRODUCT_MANAGEMENT = "/admin/products/:id",
+  CREATE_PRODUCT = "/admin/products",
+  ACCOUNT = "/account",
+  PRODUCT = "/product/:id",
+  LOGIN = "/login",
+  REGISTER = "/register",
 }
 
-export default App
+function App() {
+  const role = useAppSelector((state) => state.user.user?.role);
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path={RoutesEnum.CATALOGUE} element={<CataloguePage />} />
+        <Route path={RoutesEnum.PRODUCT} element={<ProductPage />} />
+        <Route path={RoutesEnum.ACCOUNT} element={<AccountPage />} />
+        <Route path={RoutesEnum.LOGIN} element={<LoginPage />} />
+        <Route path={RoutesEnum.REGISTER} element={<RegisterPage />} />
+        <Route path={RoutesEnum.CREATE_PRODUCT} element={<CreateProduct />} />
+        <Route path={RoutesEnum.PRODUCT_MANAGEMENT} element={<EditProduct />} />
+        {role === "admin" && (
+          <Route path={RoutesEnum.ADMIN} element={<AdminPage />} />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
